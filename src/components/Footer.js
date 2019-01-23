fecf7acbb7fc4cc69a357e9370c27424bf03e2de
Gabriel M <gabrieletudes@gmail.com>
2019-01-23 19:13:57 +0100

    Added fonts to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react'
 import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class AppFooter extends Component {
   render () {
     return (
-      <nav>
-        <NavLink to="/" activeClassName="selected">
+      <nav className="app-nav">
+        <NavLink exact to="/" className="app-nav__link" activeClassName="app-nav__link--selected">
+          <FontAwesomeIcon icon="hand-holding"size="2x" />
           Capturer
         </NavLink>
-        <NavLink to="/encyclopedie" activeClassName="selected">
+        <NavLink to="/encyclopedie" className="app-nav__link" activeClassName="app-nav__link--selected">
+          <FontAwesomeIcon icon="feather-alt" size="2x" />
           Encyclopedie
         </NavLink>
-        <NavLink to="/captures" activeClassName="selected">
+        <NavLink to="/captures" className="app-nav__link" activeClassName="app-nav__link--selected">
+          <FontAwesomeIcon icon="dove" size="2x" />
           Captures
         </NavLink>
-        <NavLink to="/utilisateurs" activeClassName="selected">
+        <NavLink to="/utilisateurs" className="app-nav__link" activeClassName="app-nav__link--selected">
+          <FontAwesomeIcon icon="users" size="2x" />
           Utilisateurs
         </NavLink>
       </nav>
