e4dfa1fccacc7e1442731932ef84406f07576eb4
Gabriel M <gabrieletudes@gmail.com>
2019-01-23 19:08:49 +0100

    Added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
+import firebase from 'firebase'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class AppHeader extends Component {
+
+  signOutUser () {
+    firebase.auth().signOut();
+  }
+
   render () {
     return (
       <header className="header">
         <h1 className="header__title">Birdy</h1>
+        <button onClick={this.signOutUser} className="color--beige">
+          <FontAwesomeIcon icon="sign-out-alt" size="2x" />
+        </button>
       </header>
     )
   }
