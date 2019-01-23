fd76a1f261bf6d93456fc1404a524f94de78d0ef
Gabriel M <gabrieletudes@gmail.com>
2019-01-23 23:32:50 +0100

    Updates

---
+++ precache-manifest.d5213d3f78148391351eb0ca6b2c842f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2596802de4472922ab73c818e2904fde",
+    "url": "/birdy-react/static/media/logo-birdy-text.2596802d.svg"
+  },
+  {
+    "revision": "a129ad9c82ca6b8eed8c",
+    "url": "/birdy-react/static/js/runtime~main.a129ad9c.js"
+  },
+  {
+    "revision": "de69bc992cd638906a1f",
+    "url": "/birdy-react/static/js/main.de69bc99.chunk.js"
+  },
+  {
+    "revision": "2f1db573495451c49c7a",
+    "url": "/birdy-react/static/js/1.2f1db573.chunk.js"
+  },
+  {
+    "revision": "de69bc992cd638906a1f",
+    "url": "/birdy-react/static/css/main.22f53fe8.chunk.css"
+  },
+  {
+    "revision": "b56f2c8583b2aeeb3db1d2c67cdea3b2",
+    "url": "/birdy-react/index.html"
+  }
+];
