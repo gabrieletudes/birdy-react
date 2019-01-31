f837b376f0eb22ba6c198e315485ac20e8079708
Gabriel M <gabrieletudes@gmail.com>
2019-01-31 12:00:21 +0100

    Added user captures amount

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -4,15 +4,36 @@ import firebase from 'firebase'
 class Users extends Component {
   state = {
     users: null,
+    single_captures: [],
+    uid: firebase.auth().currentUser.uid
   }
 
   componentDidMount(){
-    // Endpoint on the DB
-    const users = firebase.database().ref('users');
+    this.getUsersWithCaptures()
+  }
 
-    users.on('value', users => {
-      this.setState({users:  users.val()})
-    });
+  getUsersWithCaptures () {
+    // spread user
+    const allusers = {...this.state.users}
+    // define the Endpoints on the DB
+    const theusers = firebase.database().ref('users');
+    const capture_sessions = firebase.database().ref('capture_sessions');
+    // Retrives all the users
+    theusers.on('child_added', (theusers) => {
+      // store the user key
+      let userId = theusers.key;
+      //add user information and capuresamount properties to the allusers array
+      allusers[userId] = {information:'', capturesamount: null}
+      // affect the the user information to
+      allusers[userId].information = theusers.val()
+      // Retrives all the captures made by an user
+      capture_sessions.orderByChild("uid").equalTo(userId).once('value', (capturesessions) => {
+        // Add the amount of captures made by an user
+        allusers[userId].capturesamount = capturesessions.val() ? Object.values(capturesessions.val()).length : 0
+        //Add it to the state
+        this.setState({users: allusers})
+      })
+    })
   }
 
   renderUsers() {
@@ -24,13 +45,15 @@ class Users extends Component {
           {
             theusers.map((user, key) =>
               <div key={key}>
-                <h2>{user.latin_name}</h2>
                 <ul className="list-data">
                   <li className="list-data__element">
-                    <span className="text-bold h-margin-right--tiny">Nom et Prenom</span>{user.full_name}
+                    <span className="text-bold h-margin-right--tiny">Nom et Prenom</span>{user.information.full_name}
                   </li>
                   <li className="list-data__element">
-                    <span className="text-bold h-margin-right--tiny">Email</span>{user.email}
+                    <span className="text-bold h-margin-right--tiny">Email</span>{user.information.email}
+                  </li>
+                  <li className="list-data__element">
+                    <span className="text-bold h-margin-right--tiny">captures amount</span>{user.capturesamount}
                   </li>
                 </ul>
               </div>
