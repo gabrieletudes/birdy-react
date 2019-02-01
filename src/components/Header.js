43d32885afe67f87ddecb1c16338687e10c967b8
Gabriel M <gabrieletudes@gmail.com>
2019-02-01 14:00:04 +0100

    Fixed icon spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ class AppHeader extends Component {
     return (
       <header className="header">
         <h1 className="header__title">Birdy</h1>
-        <button onClick={this.signOutUser} className="color--beige">
+        <button onClick={this.signOutUser} className="btn color--beige h-padding--none">
           <FontAwesomeIcon icon="sign-out-alt" size="2x" />
         </button>
       </header>
