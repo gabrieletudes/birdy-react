f8e9a91740ed21d54c3ec30fb1fb705a2297fb8e
Gabriel M <gabrieletudes@gmail.com>
2019-06-01 11:20:44 +0200

    Added pop-up window

--- src/components/MessagePop.js
+++ src/components/MessagePop.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const MessagePop = ({message}) => {
+const MessagePop = ({message, type}) => {
+  let messageclass = type ? `pop-container__message pop-container__message--${type}` : 'pop-container__message'
   return (
     <div className="pop-container">
-      <p className="pop-container__message">{message}</p>
+      <div className={messageclass}>
+        <span onClick="" className="pop-container__message-close" aria-label="close the popup">
+          <FontAwesomeIcon icon="times"size="2x" />
+        </span>
+        <p>{message}</p>
+      </div>
     </div>
   )
 }
