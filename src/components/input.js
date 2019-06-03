e685d1fd7f10ac8ecb09d92c7464ed5358e7a230
Gabriel M <gabrieletudes@gmail.com>
2019-06-03 12:47:58 +0200

    Added error classes

--- src/components/input.js
+++ src/components/input.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
 const Input = ({label, type, name, value, onChange, ariaDescription, errormessage, defaultValue}) => {
+  let labelClasses = 'label';
+  let labelInputClasses = 'label__input'
+  let labelTextClasses = 'label__text'
+  if (errormessage) {
+    labelClasses += ' label--error';
+    labelInputClasses += ' label__input--error';
+    labelTextClasses += ' label__text--error'
+  }
   return (
-    <div className="form-group">
-      <label className="label" htmlFor={name}>
-        <input
-          type={type}
-          name={name}
-          onChange={onChange}
-          value={value}
-          className="label__input"
-          id={name}
-          aria-describedby={ariaDescription}
-          required
-          defaultValue={defaultValue}
-        />
-        <span className="label__text">{label}</span>
-      </label>
-      {errormessage && <span className="label__error">{errormessage}</span>}
+    <React.Fragment>
+      <div className="form-group">
+        <label className={labelClasses} htmlFor={name}>
+          <input
+            type={type}
+            name={name}
+            onChange={onChange}
+            value={value}
+            className={labelInputClasses}
+            id={name}
+            aria-describedby={ariaDescription}
+            required
+            defaultValue={defaultValue}
+          />
+        <span className={labelTextClasses}>{label}</span>
+        </label>
       </div>
+      {errormessage && <span className="label__error">{errormessage}</span>}
+    </React.Fragment>
   )
 }
 
